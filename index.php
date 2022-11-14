5f2c153fc2661d4aab5163faad5530c124f1af70
OussamaHaddi <oussama.turboxxx1@gmail.com>
2022-11-14 22:37:53 +0100

    add css file to my web

--- index.php
+++ index.php
@@ -3,10 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Sass/main.scss">
-    <title>Youcode clubs</title>
+    <link href="https://fonts.googleapis.com/css2?family=Abyssinica+SIL&family=Cairo:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&family=IM+Fell+English+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&family=Itim&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./Sass/main.css">
+    <title>YouCode clubs</title>
 </head>
 <body>
-    <h1>Oussama</h1>
+    
+    <section class="join_us">
+        <div class="container">
+            <div class="title">
+                <h2>Join Us</h2>
+            </div>
+            <div class="how_to_join">
+                <h3>How to Create your own <span>club</span> ?</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quas quam sint reprehenderit ad? Vel corporis aspernatur 
+                    nihil accusamus possimus, quas libero voluptatibus, inventore 
+                    aliquid labore exercitationem animi distinctio iure blanditiis.
+                </p>
+                <div class="box_btn">
+                    <div class="create_club">
+                        <img src="./Sass/images/🦆 icon _edit_.png" alt="">
+                        <strong>Create a club</strong>
+                        <p>You can create you own club by clicking at this button or click here.</p>
+                    </div>
+                    <div class="join_club">
+                        <img src="./Sass/images/🦆 icon _log out_.png" alt="">
+                        <strong>Join a club</strong>
+                        <p>You can create you own club by clicking at this button or click here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="animate_ball">
+                
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
