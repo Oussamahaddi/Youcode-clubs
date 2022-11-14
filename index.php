a366ae82aff33aaf362960ff7e9900883dda0d91
OussamaHaddi <oussama.turboxxx1@gmail.com>
2022-11-14 14:19:07 +0100

    create all file that need for my projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Sass/main.scss">
+    <title>Youcode clubs</title>
+</head>
+<body>
+    <h1>Oussama</h1>
+</body>
+</html>
